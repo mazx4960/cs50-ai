183254bb4d430fa59c35593756def08ea8374798
mazx4960 <desmondtan4561@gmail.com>
2022-01-10 09:32:16 +0800

    Update eigen.py

--- pagerank/eigen.py
+++ pagerank/eigen.py
@@ -7,7 +7,7 @@ from helper import *
 def eigen_pagerank(corpus, damping_factor):
     A = transition_matrix(corpus, damping_factor)
     w, v = eig(A)
-    if round(w[0].real) != 1:
+    if abs(w[0].real - 1) <= 0.001:
         raise Exception("Eigenvalue is not 1")
 
     result = normalize(v[:, 0].real)
